0b19d9469cf62cf6363640eed61c3e9984a9516f
Alan Ibatullin <mr.juneiak@gmail.com>
2022-08-11 16:36:55 +0300

    hotfix

--- src/components/ui/media-gatsby-image/media-gatsby-image.jsx
+++ src/components/ui/media-gatsby-image/media-gatsby-image.jsx
@@ -1,3 +1,40 @@
+import React from 'react';
+import * as styles from './media-gatsby-image.module.css';
+import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
+
+export default function MediaGatsbyImage({ image_480=false, image=false, alt='trafflab' }) {
+
+  console.log(image_480);
+  console.log(image);
+
+  const images = (typeof image === 'object' || typeof image_480 === 'object') ? withArtDirection(getImage(image ? image : image_480), [
+    {
+      media: '(max-width: 480px)',
+      image: getImage(image_480 ? image_480 : image)
+    }
+  ]) : {image_480: image_480, image: image}
+
+  console.log(images);
+
+  return (
+    (typeof image === 'object' || typeof image_480 === 'object') ? (
+      <GatsbyImage
+        image={images}
+        objectFit="fill"
+        objectPosition={'center'}
+        style={{width: "100%", height:"100%"}}
+        alt={alt}
+        formats={["auto", "webp", "avif"]}
+      />
+    ) : (
+      <picture>
+        <source media='(max-width: 480px)' srcSet={images.image_480} />
+        <img src={images.image} alt={alt} style={{width: "100%", height:"100%"}}  />
+      </picture>
+    )
+  )
+}
+
 // import * as React from "react"
 // import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
 
@@ -22,35 +59,3 @@
 //     />
 //   )
 // }
-
-import React from 'react';
-import * as styles from './media-gatsby-image.module.css';
-import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
-
-export default function MediaGatsbyImage({ image_480=false, image=false, alt='trafflab' }) {
-
-  const images = typeof image === 'object' ? withArtDirection(getImage(image ? image : image_480), [
-    {
-      media: '(max-width: 480px)',
-      image: getImage(image_480 ? image_480 : image)
-    }
-  ]) : {image_480: image_480, image: image}
-
-  return (
-    (typeof image === 'object') ? (
-      <GatsbyImage
-        image={images}
-        objectFit="fill"
-        objectPosition={'center'}
-        style={{width: "100%", height:"100%"}}
-        alt={alt}
-        formats={["auto", "webp", "avif"]}
-      />
-    ) : (
-      <picture>
-        <source media='(max-width: 480px)' srcSet={images.image_480} />
-        <img src={images.image} alt={alt} style={{width: "100%", height:"100%"}}  />
-      </picture>
-    )
-  )
-}
