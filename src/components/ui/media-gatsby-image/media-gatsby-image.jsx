0afc561c37431f92a40ed6bde9ecc62413972bb0
Alan Ibatullin <mr.juneiak@gmail.com>
2022-09-02 17:53:31 +0300

    fix images

--- src/components/ui/media-gatsby-image/media-gatsby-image.jsx
+++ src/components/ui/media-gatsby-image/media-gatsby-image.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import * as styles from './media-gatsby-image.module.css';
 import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
-import {Is480Context} from '../../../utils/contexts';
 
-export default function MediaGatsbyImage({ image_480=false, image=false, alt='traffacademy' }) {
-  const is480 = React.useContext(Is480Context);
+export default function MediaGatsbyImage({ image_480=false, image=false, alt='trafflab' }) {
 
-  // const images = (typeof image === 'object' || typeof image_480 === 'object') ? withArtDirection(getImage(image ? image : image_480), [
-  //   {
-  //     media: '(max-width: 480px)',
-  //     image: getImage(image_480 ? image_480 : image)
-  //   }
-  // ]) : {image_480: image_480, image: image}
-
-  const handledImg = getImage(image) || image;
-  const handledImg_480 = getImage(image_480) || image_480;
+  const images = (typeof image === 'object' || typeof image_480 === 'object') ? withArtDirection(getImage(image ? image : image_480), [
+    {
+      media: '(max-width: 480px)',
+      image: getImage(image_480 ? image_480 : image)
+    }
+  ]) : {image_480: image_480, image: image}
 
   return (
     (typeof image === 'object' || typeof image_480 === 'object') ? (
       <GatsbyImage
-        image={is480 ? handledImg_480 : handledImg}
+        image={images}
         objectFit="fill"
         objectPosition={'center'}
         style={{width: "100%", height:"100%"}}
@@ -28,9 +23,34 @@ export default function MediaGatsbyImage({ image_480=false, image=false, alt='tr
       />
     ) : (
       <picture>
-        <source media='(max-width: 480px)' srcSet={image_480} />
-        <img src={image} alt={alt} style={{width: "100%", height:"100%"}}  />
+        <source media='(max-width: 480px)' srcSet={images.image_480} />
+        <img src={images.image} alt={alt} style={{width: "100%", height:"100%"}}  />
       </picture>
     )
   )
 }
+
+// import * as React from "react"
+// import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
+
+// export default function MediaGatsbyImage({ image, image_480, alt='trafflab' }) {
+  
+//   const images =  withArtDirection(getImage(image ? image : image_480), [
+//     {
+//       media: '(max-width: 480px)',
+//       image: getImage(image_480 ? image_480 : image)
+//     }
+//   ])
+
+//   return (
+//     <GatsbyImage
+//       image={(image && image_480 ) ? images : (getImage(image) || getImage(image_480))}
+//       objectFit="fill"
+//       loading="eager"
+//       objectPosition={'center'}
+//       style={{width: "100%", height:"100%"}}
+//       alt={alt}
+//       formats={["auto", "webp", "avif"]}
+//     />
+//   )
+// }
