3ca10c598226d99e9f6cd6b1ba28c99395e0faa4
Alan Ibatullin <mr.juneiak@gmail.com>
2022-11-16 23:23:06 +0500

    add auto date

--- src/components/sections/opening/opening.jsx
+++ src/components/sections/opening/opening.jsx
@@ -1,53 +1,21 @@
 import * as React from "react"
 import * as styles from './opening.module.css';
 import { Is480Context, FormContext } from "../../../utils/contexts";
-import { useStaticQuery, graphql } from "gatsby";
 import { BasicButton, MediaGatsbyImage } from '../../ui/'
 import OpeningCardList from './opening-card-list/opening-card-list';
 import { GiftIcon } from "../../ui/icons";
 
+const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября',  'октября',  'ноября', 'декабря']
+
 export default function Opening({ data }) {
   const is480 = React.useContext(Is480Context);
   const openFormPopup = React.useContext(FormContext);
+  const [ eventTime, setEventTime ] = React.useState();
 
-  // const data = useStaticQuery(graphql`
-  //   query OpeningQuery {
-  //     markdownRemark {
-  //       frontmatter {
-  //         opening {
-  //           buttonText
-  //           buttonText_480
-  //           time
-  //           title {
-  //             part
-  //           }
-  //           cards {
-  //             title
-  //             text
-  //           }
-  //           giftText
-  //           giftText_480
-  //           tag
-  //           name
-  //           jobTitle
-  //           imageAlt
-  //           personImage {
-  //             childImageSharp {
-  //               gatsbyImageData(quality: 99, layout: CONSTRAINED, placeholder: BLURRED )
-  //             }
-  //           }
-  //           personImage_480 {
-  //             childImageSharp {
-  //               gatsbyImageData(quality: 99, layout: CONSTRAINED, placeholder: BLURRED)
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `).markdownRemark.frontmatter.opening;
-  
-
+  React.useEffect(() => {
+    const time = new Date(Date.now() + 86400000)
+    setEventTime(`${time.getDate()} ${months[time.getMonth()]}`);
+  }, [])
   return (
     <section id='opening' className={styles.opening}>
       <div className={styles.content}>
@@ -55,7 +23,7 @@ export default function Opening({ data }) {
         <div className={styles.textContainer}>
           <div className={styles.about}>
             <div className={styles.tag}>{data.tag}</div>
-            <pre className={styles.time}>{data.time}</pre>
+            <pre className={styles.time}>{`${eventTime} в 19:00 МСК`}</pre>
           </div>
 
           <h1 className={styles.title}>{data.title[0].part}<br/>{data.title[1].part}</h1>
